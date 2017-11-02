d726c2d2ab3308d1296c12ed67988475c0713e21
Juergen Kosel <juergen.kosel@softing.com>
2017-11-02 15:09:39 +0100

    Correct the start of the proxyd, so that it could connect to the publisher
    - Update to proxyd 1.0.2
    - Run the proxyd in an endless loop as workaround for
      https://github.com/Azure/iot-edge-opc-proxy/issues/79
    - Add a host entry, so the proxyd could resolve the publisher endpoint
      as described in
      https://github.com/Azure/iot-edge-opc-publisher/blob/master/README.md
    
    Signed-off-by: Juergen Kosel <juergen.kosel@softing.com>

--- WebApp/Views/AddGateway/startProxy.bat
+++ WebApp/Views/AddGateway/startProxy.bat
@@ -7,12 +7,19 @@
 set _HUB_CS='replace with your connect string'
 ::#Set the docker shared root variable, so that the docker containers could access the files stored on your real drive:
 ::# In the example below, it is assumed, that drive D: is a shared driver for docker, and it contains the directory docker.
-set DOCKER_SHARED_ROOT=//D/docker
+set DOCKER_SHARED_ROOT=//C/docker
 
-::# Insert your hostname and IP address to these variables:
-set MYHOSTNAME=DESKTOP-0EBERG1.example.net
-set MYIP=192.168.237.10
+::# Insert the hostname with domain of the publisher and IP address to these variables:
+set MYHOSTNAME=publisher.example.com
+set MYIP=10.123.45.26
+
+docker network create -d bridge iot_edge
 ::# Initially run the publisher to register itself at the IoT-hub:
-docker run -it --rm --name proxy -v %DOCKER_SHARED_ROOT%:/mapped microsoft/iot-edge-opc-proxy:1.0.3 -i -c "%_HUB_CS%" -D /mapped/cs.db
-::# Run the proxy permanently, so that the connected factory could connect to OPC UA servers through the proxy tunnel:
-docker run -it --rm --name proxy -v %DOCKER_SHARED_ROOT%:/mapped --add-host "%MYHOSTNAME%":%MYIP% microsoft/iot-edge-opc-proxy:1.0.3 -D /mapped/cs.db
+
+:foreverloop
+
+	docker run -it --rm --name proxy --network iot_edge -v %DOCKER_SHARED_ROOT%:/mapped microsoft/iot-gateway-opc-ua-proxy:1.0.2 -i -c "%_HUB_CS%" -D /mapped/cs.db
+	::# Run the proxy permanently, so that the connected factory could connect to OPC UA servers through the proxy tunnel:
+	docker run -it --rm --name proxy --network iot_edge -v %DOCKER_SHARED_ROOT%:/mapped --add-host "%MYHOSTNAME%":%MYIP% microsoft/iot-gateway-opc-ua-proxy:1.0.2 -D /mapped/cs.db
+
+goto foreverloop
